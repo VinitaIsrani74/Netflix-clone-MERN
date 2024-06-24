6244f97bf93feca238692de50c8e33500474264d
Vinita Israni <isranivinita74@gmail.com>
2024-06-24 21:18:34 +0530

    Setting up react and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Login from './Pages/Login/Login'
+import Signup from './Pages/Signup/Signup'
+import Netflix from './Pages/Netflix/Netflix'
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+      <Routes>
+      <Route exact path='/' element={<Netflix />}/>
+        <Route exact path='/login' element={<Login />}/>
+        <Route exact path='/signup' element={<Signup />}/>       
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
